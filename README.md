9026941d9a2725103fba7925763d5de0c1fd6876
TwinIsland <twisland@outlook.com>
2022-07-10 23:14:17 -0500

    config format change and db structure change

--- README.md
+++ README.md
@@ -4,18 +4,51 @@ The model layer class for shorten URL service
 Complete the `config.json` to meet your demand, and use the `ShoternURL` class as below
 ```python
 import ShortURL
+
 func = ShortURL.ShortURL()
 
 # add link
-result = func.add("https://sougo.com")	# 1000 items in 1.7s safe mode
+result = func.add("https://erdao.me")  # 1000 items in 1.7s safe mode
 
 # check total length
 print(func.len())
 
 # check from original link / short link
-result = func.get("https://sougo.com")
+result = func.get("https://erdao.me")
+print(result)
 
-# don't forget to close the session
+# service status
+print(func.status())
+
+# don't forget to close the session after use
 func.close()
 ```
 
+# Config
+`blacklist`: avoid adding this url
+`baseURL`: short url service base url
+`sqliteAddress`: sqlite address
+`shift_possibility`: range from (0, 1), the larger, the better effect, but may lower the efficiency with mass data
+`min_length`: min length for shorten address
+`max_length`: max length for shorten address, set as `-1` for ultimate
+`support_url_pattern`: regex validation for input url
+
+```json
+{
+   "blacklist":[
+      "sample.com"
+   ],
+   "baseURL":"base.com",
+   "sqliteAddress":"./shortLink.db",
+   "shift_possibility": 0.9,
+   "min_length": 4,
+   "max_length": -1,
+   "support_url_pattern":"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+}
+```
+
+# Status Code
+`NONE`: nothing find
+`ILLEGAL`: input is illegal
+`ERROR`: service error, maybe caused by db closed
+`FULL`: db excess maximum amount
--- ShortURL.py
+++ ShortURL.py
@@ -11,48 +11,57 @@ import sqlite3 as sq
 import re
 import json
 import os
+import time
 
 
 class ShortURL:
     def __init__(self):
         f = json.load(open("config.json", "r"))
-        sqliteAddress = f["config"]["sqliteAddress"]
-        self.__config = f
-        self.__baseURL = f["config"]["baseURL"]
-        self.__shift_possibility = f["config"]["shift_possibility"]
-        self.__blacklist = f["config"]["blacklist"]
-        self.__support_url_pattern = f["config"]["support_url_pattern"]
+        self.__sqliteAddress = f["sqliteAddress"]
+        self.__baseURL = f["baseURL"]
+        self.__shift_possibility = f["shift_possibility"]
+        self.__blacklist = f["blacklist"]
+        self.__support_url_pattern = f["support_url_pattern"]
         self.__pattern = re.compile(self.__support_url_pattern)
-
-        if not os.path.exists(sqliteAddress):
-            con = sq.connect(sqliteAddress)
+        self.__min_length = f["min_length"]
+        self.__max_length = f["max_length"]
+        self.__max_length = 5 if self.__max_length == -1 else self.__max_length
+        if self.__min_length > self.__max_length or self.__min_length <= 0 \
+                or self.__shift_possibility <= 0\
+                or self.__shift_possibility >= 1:
+            raise ValueError("value invalid, check your config")
+        self.__shift_total = [36 ** i for i in range(self.__min_length, self.__max_length + 1)]
+        if not os.path.exists(self.__sqliteAddress):
+            con = sq.connect(self.__sqliteAddress)
             db = con.cursor()
             try:
                 db.execute('''
                 CREATE TABLE LINKS
                        (ID INTEGER PRIMARY KEY AUTOINCREMENT,
                        LINK CHAR(50)  UNIQUE NOT NULL,
-                       RED CHAR(5) UNIQUE NOT NULL );''')
+                       RED CHAR(5) UNIQUE NOT NULL ,
+                       TIME CHAR(10) NOT NULL)''')
                 con.commit()
                 con.close()
             except Exception as e:
                 raise Exception("Fail to initialize database: " + str(e))
 
-        self.__con = sq.connect(sqliteAddress)
+        self.__con = sq.connect(self.__sqliteAddress)
         self.__db = self.__con.cursor()
         self.__tLength = [i[0] for i in self.__db.execute("SELECT Count(*) FROM LINKS")][0]
+        self.__connect_status = True
 
     def __generate_rand_phase(self):
-        pos = [1190,
-               39270,
-               1256640,
-               38955840]
-        length = 0
-        for i in range(len(pos)):
-            if (pos[i] - self.__tLength) / pos[i] > self.__shift_possibility:
-                length = i + 2
+        db_full = True
+        length = self.__min_length
+        for i in range(len(self.__shift_total)):
+            if (self.__shift_total[i] - self.__tLength) / self.__shift_total[i] > self.__shift_possibility:
+                db_full = False
                 break
+            length += 1
         result = ""
+        if db_full:
+            return False
         for i in range(length):
             result += chr(random.randint(ord('a'), ord('z'))) if random.randint(0, 1) else str(random.randint(0, 9))
         return result
@@ -72,35 +81,62 @@ class ShortURL:
         return self.__tLength
 
     def add(self, link):
+        if not self.__connect_status:
+            return "ERROR"
         if not self.__check_legal_url(link):
             return "ILLEGAL"
         r = [i[0] for i in self.__db.execute("SELECT RED from LINKS WHERE LINK = '{}'".format(link))]
         if len(r) != 0:
             return r[0]
-        red = self.__baseURL + self.__generate_rand_phase()
+        red = self.__generate_rand_phase()
+        if not red:
+            return "FULL"
         while len([i[0] for i in self.__db.execute("SELECT * from LINKS WHERE RED = '{}'".format(red))]) != 0:
-            red = self.__baseURL + self.__generate_rand_phase()
-        self.__db.execute("INSERT INTO LINKS (LINK, RED) \
-                   VALUES ('{link}','{red}')".format(link=link, red=red))
+            red = self.__generate_rand_phase()
+        self.__db.execute("INSERT INTO LINKS (LINK, RED, TIME) \
+                   VALUES ('{link}','{red}', '{time}')".format(link=link, red=red, time=int(time.time())))
         self.__con.commit()
         self.__tLength += 1
         return red
 
     def get(self, url):
+        if not self.__connect_status:
+            return "ERROR"
         if not self.__check_legal_url(url):
             return "ILLEGAL"
         mode = ["RED", "LINK"]
         if self.__baseURL in url:  # red -> link
             mode = ["LINK", "RED"]
+            url = url.split("/")[-1]
         r = [i[0] for i in self.__db.execute("SELECT {mode1} from LINKS WHERE {mode2} = '{url}'"
                                              .format(url=url, mode1=mode[0], mode2=mode[1]))]
         if len(r) != 0:
             return r[0]
-        return "None"
+        return "NONE"
+
+    def status(self):
+        return {"total": self.__tLength,
+                "db": self.__sqliteAddress,
+                "connection": "on" if self.__connect_status else "off",
+                "blacklist": self.__blacklist,
+                "slot": [int((1 - self.__shift_possibility) * i) for i in self.__shift_total]
+                }
+
+    def connect(self):
+        if self.__connect_status:
+            print("already connect")
+        else:
+            self.__con = sq.connect(self.__sqliteAddress)
+            self.__db = self.__con.cursor()
+            self.__tLength = [i[0] for i in self.__db.execute("SELECT Count(*) FROM LINKS")][0]
+            self.__connect_status = True
+            print("reconnect success")
 
     def close(self):
-        with open("config.json", "w") as f:
-            self.__config["count"] = self.__tLength
-            json.dump(self.__config, f)
-        f.close()
         self.__con.close()
+        self.__connect_status = False
+        print("close successful")
+
+
+if __name__ == "__main__":
+    print("module loaded")
